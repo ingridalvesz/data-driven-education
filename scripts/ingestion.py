9d99946aebf30b11382f94c0b0b5b48bbcf3e03e
Ingrid Alves <88692119+ingridalvesz@users.noreply.github.com>
2025-09-26 12:22:27 -0300

    Remover dados desnecessários

--- scripts/ingestion.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from config import PARTICIPANTES_RAW, NOTA_RAW
-
-def load_participantes() -> pd.DataFrame:
-    return pd.read_parquet(PARTICIPANTES_RAW)
-
-def load_nota() -> pd.DataFrame:
-    return pd.read_parquet(NOTA_RAW)
